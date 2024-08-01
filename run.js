2228520ec4e6a4c26c102ffe984a0f7fe03a1b08
todorokiv1 <155774992+todorokiv1@users.noreply.github.com>
2024-08-01 17:17:42 +0100

    Create run.js

---
+++ run.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const { exec } = require('child_process');
+
+const app = express();
+const PORT = process.env.PORT || 2000;
+
+const server = app.listen(PORT, () => {
+  console.log('Your app is listening on port ' + PORT);
+  console.log("I'm Ready To Work..! 24H");
+});
+
+app.get('/', (req, res) => {
+  res.send(`
+    <body>
+      <center><h1>Bot 24H ON!</h1></center>
+    </body>
+  `);
+});
+
+exec('concurrently "node command.js" "node answer.js"', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+  if (stderr) {
+    console.error(`stderr: ${stderr}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
+
+process.on('SIGINT', () => {
+  console.log('Server shutting down...');
+  server.close(() => {
+    console.log('Server shut down successfully.');
+    process.exit(0);
+  });
+});
